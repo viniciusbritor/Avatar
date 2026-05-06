df09f1065a8f032386aa1589bafdeb439133f598
Vinicius Brito Rocha <viniciusbritor@gmail.com>
2026-05-06 03:18:58 -0300

    chore: remover check.py injetado sem querer

--- check.py
+++
@@ -1,5 +0,0 @@
-import json
-from google.cloud import firestore
-db = firestore.Client(project='brasili-ia-news')
-d = db.collection('avatar_jobs').document('82d63a15').get().to_dict()
-print(json.dumps({'job_id': '82d63a15', 'status': d.get('status'), 'video': d.get('video_path')}, indent=2))
