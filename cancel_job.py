8e7cf038c2417a890f35f785b236863de18be30b
Vinicius Brito Rocha <viniciusbritor@gmail.com>
2026-05-06 18:02:11 -0300

    chore: remover scripts temporarios de diagnostico

--- cancel_job.py
+++
@@ -1,7 +0,0 @@
-from google.cloud import firestore
-db = firestore.Client(project='brasili-ia-news')
-# Cancela job afetado
-db.collection('avatar_jobs').document('cbde490f').update({
-    'status': 'cancelled',
-    'message': 'Cancelado devido ao bug de injeção de metadata antigo.'
-})
